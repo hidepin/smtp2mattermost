c4998e9f75ca1f6033eef68d9f2d24789c4b38d3
Hidetoshi Imai <hidepin@gmail.com>
2018-04-09 07:47:26 +0900

    Add decode function.

--- smtp2mattermost.py
+++ smtp2mattermost.py
@@ -2,6 +2,9 @@
 
 import smtpd
 import asyncore
+import re
+import quopri
+from email.header import decode_header
 
 class CustomSMTPServer(smtpd.SMTPServer):
     def __init__(self, localaddr, remoteaddr, data_size_limit=33554432, map=None, enable_SMTPUTF8=False, decode_data=False):
@@ -15,9 +18,15 @@ class CustomSMTPServer(smtpd.SMTPServer):
         print('Message length        :', len(data))
         (header, body) = data.split('\n\n\n')
         print('---------- header ----------')
-        print('Message header  :', header)
+        for line in header.split('\n'):
+            pattern = re.compile("Subject")
+            match = pattern.match(line)
+            if match != None:
+                print('Subject  :', decode_header(line)[1][0].decode('utf-8'))
+            else:
+                print(line)
         print('---------- body ----------')
-        print('Message body  :', body)
+        print(quopri.decodestring(body).decode('utf-8'))
         return
 
 server = CustomSMTPServer(('0.0.0.0', 1025), None, decode_data=True)
