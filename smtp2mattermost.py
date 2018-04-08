db3ed100494b882ecd055bb792d5b78f31869070
Hidetoshi Imai <hidepin@gmail.com>
2018-04-09 06:47:22 +0900

    Support utf-8.

--- smtp2mattermost.py
+++ smtp2mattermost.py
@@ -4,16 +4,18 @@ import smtpd
 import asyncore
 
 class CustomSMTPServer(smtpd.SMTPServer):
-    def process_message(self, peer, mailfrom, rcpttos, data, mail_options, rcpt_options):
+    def __init__(self, localaddr, remoteaddr, data_size_limit=33554432, map=None, enable_SMTPUTF8=False, decode_data=False):
+        # 再利用している
+        super(CustomSMTPServer, self).__init__(localaddr, remoteaddr, data_size_limit, map, enable_SMTPUTF8, decode_data)
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
         print('Receiving message from:', peer)
         print('Message addressed from:', mailfrom)
         print('Message addressed to  :', rcpttos)
         print('Message length        :', len(data))
         print('Message data  :', data)
-        print('Message mail_options  :', mail_options)
-        print('Message rcpt_options  :', rcpt_options)
         return
 
-server = CustomSMTPServer(('192.168.0.9', 1025), None)
+server = CustomSMTPServer(('192.168.0.9', 1025), None, decode_data=True)
 
 asyncore.loop()
